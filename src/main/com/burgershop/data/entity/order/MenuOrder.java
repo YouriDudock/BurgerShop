ff528d5775ac08c6e101e10c96674c1094523c06
YouriDudock <youridudock@gmail.com>
2022-01-24 22:54:35 +0100

    Order doc

--- src/main/com/burgershop/data/entity/order/MenuOrder.java
+++ src/main/com/burgershop/data/entity/order/MenuOrder.java
@@ -3,10 +3,15 @@ package main.com.burgershop.data.entity.order;
 import main.com.burgershop.data.model.order.Order;
 
 /**
+ * A menu order is an order that contains a preset of addition ids combined with a burger
+ *
  * @author Youri Dudock
  */
 public class MenuOrder extends Order {
 
+    /**
+     * Id of the menu
+     */
     private final int menuId;
 
     public MenuOrder(int menuId, int burgerId, int[] additionIds) {
--- src/main/com/burgershop/data/entity/order/OrderAddition.java
+++ src/main/com/burgershop/data/entity/order/OrderAddition.java
@@ -3,14 +3,25 @@ package main.com.burgershop.data.entity.order;
 import java.math.BigDecimal;
 
 /**
+ * An order addition is an addition to the order besides the burger, this could be things like chips etc
+ *
  * @author Youri Dudock
  */
 public class OrderAddition {
 
+    /**
+     * Id of the order addition
+     */
     private final int id;
 
+    /**
+     * Name of the order addition
+     */
     private final String name;
 
+    /**
+     * Price of the order addition
+     */
     private final BigDecimal price;
 
     public OrderAddition(int id, String name, BigDecimal price) {
